2f741bc430688f29b5e85b33a6f14b4c1acc84b3
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-07-15 11:11:29 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,33 @@
+
+/***** Erinnerung: Arrays *******/
+
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
+
+// // nested arrays
+// arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
+// output(arr[0][2]);
+// output(arr[1][2]);
+// output("------------");
+
+// // nested loops
+// for (let i = 0; i < arr.length; i++) {
+//        for (let j = 0; j < arr[0].length; j++) { 
+//         output(arr[i][j]);          
+//        }   
+// }
+
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+
+
+
+
+/** Ausgabe */
+function output(inputData) {
+        console.log(inputData);
+}
