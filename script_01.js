905217a0966e511bde643bfe21e1ce4c2d53cd87
Roman <roman.makhanbetov@gmx.de>
2022-07-15 12:56:08 +0200

    Objekte 2 - Hierarchie

--- script_01.js
+++ script_01.js
@@ -46,7 +46,30 @@ let person =
 //                 person.salary[1] + " p.a.";
 
 // output(txt);
-output(person.sayHello());
+//output(person.sayHello());
+
+
+/***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+
+const baikal = 
+{
+        value: "10m",
+        deep: 
+        {
+           deeper:
+           {
+                deepest: "Das Licht - auf 1642m!"
+           }
+        }
+};
+
+
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
+
 
 /** Ausgabe */
 function output(inputData) {
